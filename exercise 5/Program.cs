4a6360243a0e85a8fc53571def347130dcca8d5f
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-12-21 16:53:35 +0700

    add method class

--- exercise 5/Program.cs
+++ exercise 5/Program.cs
@@ -61,6 +61,29 @@ namespace exercise_5
             }
             Node display;
         }
-
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            char ch;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1. Implement insert operation");
+                    Console.WriteLine("2. Implement delete operation");
+                    Console.WriteLine("3. Display values");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\nEnter your choice (1-4): ");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Check for the values entered.");
+                }
+            }
+        }
     }
 }
